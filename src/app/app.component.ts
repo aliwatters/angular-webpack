98eea15132c8140a66be166cad1c3d0744edc547
Ali Watters <ali.watters@gmail.com>
2016-09-25 17:41:10 -0700

    initial commit - working webpack

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'src/app/app.component.html',
+  styleUrls: ['src/app/app.component.css']
+})
+export class AppComponent { }
